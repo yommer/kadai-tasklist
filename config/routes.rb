f7d3f2a71b83ead1bca950246a404d47759af078
keiko.yoshida <kend.0.yommer@gmail.com>
2018-08-08 20:42:31 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'signup', to: 'tasks#index'
+  get 'signup', to: 'users#new'
   resources :tasks
   
   resources :users, only: [:index, :show, :new, :create]
